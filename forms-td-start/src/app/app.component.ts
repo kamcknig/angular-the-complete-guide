e702ed2da64bb500796715c39f6cfcf3574724ee
Kyle McKnight <kyle.mcknight@accesso.com>
2018-09-27 10:36:42 -0400

    start of section 15, not completed

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component, ElementRef} from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  answer: string = '';
+  genders = ['male', 'female'];
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
