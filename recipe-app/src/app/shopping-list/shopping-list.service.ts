acb35c7933e78d0843c8c4b05023d8c7afba85ff
Kyle McKnight <kyle.mcknight@accesso.com>
2018-09-27 09:51:30 -0400

    section 14 recipe app obserables complete

--- recipe-app/src/app/shopping-list/shopping-list.service.ts
+++ recipe-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,12 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Ingredient} from "../shared/ingredient.model";
+import {Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
-  ingredientsChanged: EventEmitter<Ingredient[]> = new EventEmitter<Ingredient[]>();
+  ingredientsChanged: Subject<Ingredient[]> = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apple', 5),
@@ -20,12 +21,12 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     console.log("Adding", ingredients, "to shopping list");
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
